875b7a16df786de83dba2bd98afe50fd3b190aa0
ku-rehan <usama.rehan@pudding.app>
2020-02-12 12:13:53 +0530

    Updated PHPDocs for ProjectController

--- app/Http/Controllers/ProjectController.php
+++ app/Http/Controllers/ProjectController.php
@@ -4,18 +4,25 @@ namespace App\Http\Controllers;
 
 use App\Http\Resources\ProjectResource;
 use App\Projects;
-use Illuminate\Http\Request;
+use Illuminate\Http\Resources\Json\AnonymousResourceCollection;
 
 class ProjectController extends Controller
 {
-    //
+    /**
+     * Function to display all the Project Resources available
+     * @return AnonymousResourceCollection
+     */
     public function index() {
 
         return ProjectResource::collection(Projects::with(['cases', 'notes', 'tasks'])->get());
 
     }
 
-    //
+    /**
+     * Function to display the requested Project Resources
+     * @param Projects $project
+     * @return ProjectResource
+     */
     public function show(Projects $project) {
 
         return new ProjectResource($project->load(['cases', 'notes', 'tasks']));
